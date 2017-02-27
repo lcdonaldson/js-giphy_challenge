3fd57c9637104f3910e103211ec341c9a5b19d47
Levi Donaldson <lcdbacanas@gmail.com>
2017-02-26 22:13:22 -0600

    giphy api search added

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 $(document).ready(function (){
-    $("#name").on("change", function(event){
+    $("button").on("click", function(event){
         event.preventDefault();
-        console.log('hey');
+
+        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5");
+        // var img = $('<img/>', {src: xhr + '.png'});
+        // img.appendTo('.content');
+
+        xhr.done(function(data) { 
+            // $('<p>' +  xhr + '</p>').appendTo('.content');
+            console.log(data);
+            var data = data.data[0].images.original.url;
+            // var result = drawImage(data);
+            document.getElementById("results").innerHTML = data; 
+
+        });
+
+
+        // var written = $("#name").val();
+        // $('<p>' +  written + '</p>').appendTo('.content');
+
+
+
+        // var api = "http://api.giphy.com/v1/gifs/search?";
+        // var apiKey = "&api_key=dc6zaTOxFJmzC";
+        // var query = "&q=ryan+gosling";
+
+        // function setup() {
+        //     noCanvas();
+        //     var url = api + apiKey + query;
+        //     loadJSON(url, gotData);
+        // };
+
+        // function gotData(data) {
+        //     println(data.data[0].images.original.url)
+        // };
+
+                
     });
-})
+
+    
+
+});
