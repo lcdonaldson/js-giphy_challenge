28a6e64d20f89bf54190b411b355da9ff794c91f
Levi Donaldson <lcdbacanas@gmail.com>
2017-02-28 09:49:45 -0600

    final edits

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 consume giphy api and display results of search.
 
 Here I was issued a challenge to consume the API from giphy.com to search
-and present giphs to the page 25 at a time. Also I needed to load 25 more results as I scrolled to the bottom of the results.
+and present giphs to the page 20 at a time. Also I needed to load 20 more 
+results as I scrolled to the bottom of the results.
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var placeholder;
 
     function makeGiphyCall(offset) {
+        // Set the variables
         var api = "http://api.giphy.com/v1/gifs/search?";
         var apiKey = "&api_key=dc6zaTOxFJmzC";
         var written = $("#name").val();
@@ -12,13 +13,14 @@ $(document).ready(function () {
             placeholder = written;
             offset = 20;
         };
-
+        // Get the data from search input
         var query = "&q=" + written + "&limit=20&offset=" + offset;
         var url = api + apiKey + query;
         var xhr = $.get(url);
 
-        xhr.done(function(giphy) {
         // When the request has been made, and you get a response
+        // Iterate through results and place on the page.
+        xhr.done(function(giphy) {
             for (var i = 0; i < giphy.data.length; i++) {
                 var data = giphy.data[i].images.downsized.url;
                 var result = $('.content').append("<img src='" + data + "'/>");
@@ -27,11 +29,13 @@ $(document).ready(function () {
         });
     }
 
+    // click event which executes call to function above
     $("button").on("click", function(event){
         event.preventDefault();
 
         makeGiphyCall();
-
+        // Load next set of images when scrolled to bottom of
+        // the page
         $(window).scroll(function() {
             if($(window).scrollTop() == $(document).height() - $(window).height()) {
                 makeGiphyCall(offset);
